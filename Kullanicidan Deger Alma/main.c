37c49b000dd3326887de2f525fe6809bdb118b1e
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-10 13:21:52 +0300

    Kullanıcıdan Değer Alma

---
+++ Kullanicidan Deger Alma/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*
+{
+    int a;
+    printf("Lutfen bir deger giriniz\n");
+    scanf("%d",&a);
+    printf("Sayim %d",a);
+}
+*/
+/*
+{
+    float a;
+    printf("lutfen bir deger giriniz\n");
+    scanf("%f",&a);
+    printf("Sayim %f",a);
+}
+*/
+/*
+{
+    char a;
+    printf("Lutfen bir karakter giriniz\n");
+    scanf("%c",&a);
+    printf("Karakterim %c", a);
+}
+*/
+/*
+{
+    char a[10];
+    printf("Lutfen bir karakter dizisi giriniz\n");
+    scanf("%c",&a);
+    printf("Karakter dizim %s", a);
+}
+*/
+
+{
+    int a;
+    int b;
+    printf("Lutfen bir sayi giriniz\n");
+    scanf("%d",&a);
+    printf("Lutfen bir sayi giriniz\n");
+    scanf("%d",&b);
+    printf("Sayi toplamim %d",a+b);
+}
