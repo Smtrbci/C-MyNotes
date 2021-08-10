2021f568048d3f383e1be22e274579cbf1d996f3
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-10 16:10:38 +0300

    Derleyici Yorum Yazma

---
+++ Derleyici Yorum Yazma/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int a;
+
+printf("Lutfen bir sayi giriniz\n");
+ scanf("%d",a);
+
+ /*
+printf("Lutfen bir sayi giriniz\n");
+ scanf("%d",a);
+ */
+
+  //printf("Lutfen bir sayi giriniz\n");
+ scanf("%d",a);
+}
