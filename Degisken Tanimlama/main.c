65d7675430a5b726e6535a1b2bc5fa4690161dad
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-10 13:21:03 +0300

    Değişken Tanımlama

---
+++ Degisken Tanimlama/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*
+{
+    printf("Hos geldin");
+}
+
+*/
+/*
+{
+    int a=4;
+    printf("%d Hos geldin", a);
+}
+*/
+{
+    char a='h';
+    printf("%c Hos geldin", a);
+}
