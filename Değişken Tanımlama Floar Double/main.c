5e596bfdb6180da1460cbe92152d326d9949dba3
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-10 14:02:21 +0300

    Değişken tanımlama Float Double

---
+++ Değişken Tanımlama Floar Double/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*
+{
+    int sayi=3.14;
+    printf("%d\n",sayi);
+    return 0;
+}
+*/
+/*
+{
+    float sayi=3.14;
+    printf("%f\n",sayi);
+    return 0;
+}
+*/
+/*
+{
+    float sayi=3.14;
+    printf("%.2f\n",sayi);
+    return 0;
+}
+*/
+/*
+{
+    int tamsayi;
+    char karakter;
+    float ondaliksayi1;
+    double ondaliksayi2;
+
+    printf("int bellkte %d byte kadar yer tutar\n", sizeof(int));
+    printf("char bellkte %d byte kadar yer tutar\n", sizeof(char));
+    printf("float bellkte %d byte kadar yer tutar\n", sizeof(float));
+    printf("double bellkte %d byte kadar yer tutar\n", sizeof(double));
+    return 0;
+}
+*/
+{
+    int tamsayi=1;
+    char karakter='c';
+    float ondaliksayi1=3.15;
+    double ondaliksayi2=789.45;
+
+    printf("int bellkte %d byte kadar yer tutar\n", sizeof(int));
+    printf("char bellkte %d byte kadar yer tutar\n", sizeof(char));
+    printf("float bellkte %d byte kadar yer tutar\n", sizeof(float));
+    printf("double bellkte %d byte kadar yer tutar\n", sizeof(double));
+    return 0;
+}
