00bad42f1f3f642ce71086a77a1ac41588830edf
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-10 17:03:22 +0300

    İşlem Oparatörleri

---
+++ Islem Oparatorleri/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+    + - * / %
+
+*/
+
+int main()
+/*
+{
+    int a;
+    int b;
+
+    printf("Lutfen bir sayi grirniz\n");
+    scanf("%d",&a);
+    printf("Lutfen bir sayi grirniz\n");
+    scanf("%d",&b);
+    printf("Sayinin sonucu %d",a+b);
+}
+*/
+/*
+{
+    float a;
+    float b;
+
+    printf("Lutfen bir sayi grirniz\n");
+    scanf("%f",&a);
+    printf("Lutfen bir sayi grirniz\n");
+    scanf("%f",&b);
+    printf("Sayinin sonucu %.2f",a/b);
+}
+*/
+{
+    int a;
+    int b;
+
+    printf("Lutfen bir sayi grirniz\n");
+    scanf("%d",&a);
+    printf("Lutfen bir sayi grirniz\n");
+    scanf("%d",&b);
+    printf("Sayinin sonucu %f",(float)a+b);
+}
