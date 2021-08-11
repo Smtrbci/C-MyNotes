141ffd752ec15cdf0f6587f3efe10a26c1f6b550
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-11 15:32:34 +0300

    Karşılaştırma Oparatörleri

---
+++ Karsilastirma Oparatorleri/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  == , != , > , < , <= , >= */
+
+int main()
+/*
+{
+    int a;
+    printf("Lutfen bir deger giriniz\n");
+    scanf("%d",&a);
+
+    if(a==4)
+    {
+        printf("Programlama mantigini anliyorum.");
+    }
+}
+*/
+
+/*
+{
+        int a;
+    printf("Lutfen bir deger giriniz\n");
+    scanf("%d",&a);
+
+    if(a!=4)
+    {
+        printf("Programlama mantigini anliyorum.");
+    }
+}
+*/
+
+/*
+{
+    int a;
+    printf("Lutfen bir deger giriniz\n");
+    scanf("%d",&a);
+
+    if(a>4)
+    {
+        printf("Programlama mantigini anliyorum.");
+    }
+}
+*/
+
+/*
+{
+    int a;
+    printf("Lutfen bir deger giriniz\n");
+    scanf("%d",&a);
+
+    if(a<4)
+    {
+        printf("Programlama mantigini anliyorum.");
+    }
+}
+*/
+
+/*
+{
+        int a;
+    printf("Lutfen bir deger giriniz\n");
+    scanf("%d",&a);
+
+    if(a<=4)
+    {
+        printf("Programlama mantigini anliyorum.");
+    }
+}
+*/
+
+{
+        int a;
+    printf("Lutfen bir deger giriniz\n");
+    scanf("%d",&a);
+
+    if(a>=4)
+    {
+        printf("Programlama mantigini anliyorum.");
+    }
+}
