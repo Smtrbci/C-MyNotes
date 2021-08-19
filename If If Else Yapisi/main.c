0d29dbd519685d955b6711d89b87ce480fb8f1a2
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-19 20:23:01 +0300

    If If Else Yapısı

---
+++ If If Else Yapisi/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*
+{
+    int a=5;
+
+    if (a>0)
+    {
+        printf("%d",a);
+    }
+    return 0;
+}
+*/
+/*
+{
+    int a=5;
+
+    if ((a%2)==1)
+    {
+        printf("%d",a);
+    }
+    return 0;
+}
+*/
+/*
+{
+    int a=6;
+    int b=6;
+
+    if (a==b)
+    {
+        printf("Iki sayi birbirine esittir");
+    }
+    return 0;
+}
+*/
+{
+    int a=4;
+
+    if (a>0)
+    {
+        printf("Sayim positiftir",a);
+    }
+    else
+    {
+        printf("Sayim negatiftir");
+    }
+    return 0;
+}
