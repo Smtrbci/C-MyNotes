bad535fad740a5516b22ee3f3c9e25b35674e5ff
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-22 16:37:29 +0300

    İf Eşitttir En Sık Yapılan Hata

---
+++ If Esitttir En Sik Yapilan Hata/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*
+{
+    int x;
+    x==0;
+    if(x=0)
+    {
+        printf("Bravo");
+    }
+    return 0;
+}
+*/
+{
+    int x;
+    x=0;
+    if(x==0)
+    {
+        printf("Bravo");
+    }
+    return 0;
+}
